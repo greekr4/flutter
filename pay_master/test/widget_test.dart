577b96ce5bf3b1bb1860b17ed8d4acfdfbf16ec6
greekr4 <greekr4@naver.com>
2023-08-03 17:53:14 +0900

    230803

--- pay_master/test/widget_test.dart
+++ pay_master/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:fly_game/main.dart';
+import 'package:pay_master/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
